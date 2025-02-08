937b69ffcc08b8dd2bf8992f31c813ef121f70fd
msramalho <19508417+msramalho@users.noreply.github.com>
2025-02-08 23:33:02 +0000

    new schemas

--- src/db/schemas.py
+++ src/db/schemas.py
@@ -7,12 +7,12 @@ from datetime import datetime
 class SubmitSheet(BaseModel):
     sheet_id: str | None
     author_id: str | None = None
-    group_id: str | None
+    group_id: str = "default"
     tags: set[str] | None = set()
     columns: dict | None = {}  # TODO: implement/remove
 
 
-class SubmitManual(BaseModel):
+class SubmitManual(BaseModel):  # deprecated
     result: str  # should be a Metadata.to_json()
     public: bool = False
     author_id: str | None = None
@@ -54,7 +54,7 @@ class TaskDelete(Task):
     deleted: bool
 
 
-class ActiveUser(BaseModel):    
+class ActiveUser(BaseModel):
     active: bool
 
 
@@ -78,12 +78,14 @@ class ArchiveTrigger(BaseModel):
     group_id: Annotated[str, Len(min_length=1)] = "default"
     tags: set[str] | None = None
 
+
 class ArchiveCreate(ArchiveTrigger):
     id: str | None = None
     result: dict | None = None
     sheet_id: str | None = None
     urls: list | None = None
     store_until: datetime | None = None
+
 
 class Archive(ArchiveCreate):
     created_at: datetime
@@ -98,11 +100,18 @@ class Usage(BaseModel):
     monthly_mbs: int = 0
     total_sheets: int = 0
 
+
 class UsageResponse(Usage):
     groups: dict[str, Usage]
+
 
 class CelerySheetTask(BaseModel):
     success: bool
     sheet_id: str
     time: datetime
     stats: dict
+
+
+class SubmitManualArchive(ArchiveTrigger):
+    url: None = None
+    result: str  # should be a Metadata.to_json()
