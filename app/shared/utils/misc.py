e0bd050e7a7f4b47a35a93ef4ac96fcb91f8dcdc
michplunkett <5885605+michplunkett@users.noreply.github.com>
2025-03-03 12:03:05 -0600

    Merge branch 'dev' into ruff-shared

--- app/shared/utils/misc.py
+++ app/shared/utils/misc.py
@@ -1,9 +1,14 @@
 def fnv1a_hash_mod(s: str, modulo: int) -> int:
-    # receives a string and returns a number in [0:modulo-1], ensures an even distribution over the modulo range
-    hash = 0x811C9DC5  # FNV offset basis
+    # receives a string and returns a number in [0:modulo-1], ensures an even
+    # distribution over the modulo range
+    offset_basis_hash = 0x811C9DC5  # FNV offset basis
     fnv_prime = 0x01000193  # FNV prime
     for char in s:
-        hash ^= ord(char)
-        hash *= fnv_prime
-        hash &= 0xFFFFFFFF  # Keep it 32-bit
-    return (hash if hash < 0x80000000 else hash - 0x100000000) % modulo
+        offset_basis_hash ^= ord(char)
+        offset_basis_hash *= fnv_prime
+        offset_basis_hash &= 0xFFFFFFFF  # Keep it 32-bit
+    return (
+        offset_basis_hash
+        if offset_basis_hash < 0x80000000
+        else offset_basis_hash - 0x100000000
+    ) % modulo
