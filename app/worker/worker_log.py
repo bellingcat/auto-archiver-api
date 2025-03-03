b5571fc61fae667a7973125ae04121876e83764b
Michael Plunkett <5885605+michplunkett@users.noreply.github.com>
2025-03-03 11:59:04 -0600

    Format and lint `workers` directory (#60)

--- app/worker/worker_log.py
+++ app/worker/worker_log.py
@@ -1,6 +1,5 @@
 import sys
 
-from celery import Celery
 from loguru import logger
 
 from app.shared.task_messaging import get_celery
@@ -8,24 +7,32 @@ from app.shared.task_messaging import get_celery
 
 celery = get_celery("worker")
 
-def setup_celery_logger(celery):
-	# Remove Celery's default handlers to prevent duplicate logs
-	celery_logger = celery.log.get_default_logger()
-	for handler in celery_logger.handlers[:]:
-		celery_logger.removeHandler(handler)
 
-	# Set up Loguru logging
-	logger.add("logs/celery_logs.log", retention="30 days", level="DEBUG")
-	logger.add("logs/celery_error_logs.log", retention="30 days", level="ERROR")
+def setup_celery_logger(c):
+    # Remove Celery's default handlers to prevent duplicate logs
+    celery_logger = c.log.get_default_logger()
+    for handler in celery_logger.handlers[:]:
+        celery_logger.removeHandler(handler)
 
-	# Redirect Celery logs to Loguru
-	class InterceptHandler:
-		def write(self, message):
-			if message.strip():
-				logger.info(message.strip())
-		# Required to prevent issues with buffered output
-		def flush(self): pass
-		def isatty(self): return False
+    # Set up Loguru logging
+    logger.add("logs/celery_logs.log", retention="30 days", level="DEBUG")
+    logger.add("logs/celery_error_logs.log", retention="30 days", level="ERROR")
 
-	sys.stdout = InterceptHandler()
-	sys.stderr = InterceptHandler()
+    # Redirect Celery logs to Loguru
+    class InterceptHandler:
+        @staticmethod
+        def write(message):
+            if message.strip():
+                logger.info(message.strip())
+
+        # Required to prevent issues with buffered output
+        @staticmethod
+        def flush():
+            pass
+
+        @staticmethod
+        def isatty():
+            return False
+
+    sys.stdout = InterceptHandler()
+    sys.stderr = InterceptHandler()
