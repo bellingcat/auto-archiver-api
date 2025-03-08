415a9b1644076a6851174e48f078285b8d0a4fcd
michplunkett <5885605+michplunkett@users.noreply.github.com>
2025-03-07 22:50:23 -0600

    Update misc.py

--- app/web/utils/misc.py
+++ app/web/utils/misc.py
@@ -3,7 +3,7 @@ import base64
 from fastapi.encoders import jsonable_encoder
 
 
-def custom_jsonable_encoder(obj) -> str:
+def custom_jsonable_encoder(obj):
     if isinstance(obj, bytes):
         return base64.b64encode(obj).decode("utf-8")
     return jsonable_encoder(obj)
