bbf111ac29abbf838b8b939bfc203f8d01652056
michplunkett <5885605+michplunkett@users.noreply.github.com>
2025-03-26 22:21:01 -0500

    task

--- app/web/main.py
+++ app/web/main.py
@@ -14,7 +14,7 @@ from app.web.middleware import logging_middleware
 from app.web.routers.default import router as default_router
 from app.web.routers.interoperability import router as interoperability_router
 from app.web.routers.sheet import router as sheet_router
-from app.web.routers.task import task_router
+from app.web.routers.task import router as task_router
 from app.web.routers.url import url_router
 from app.web.security import token_api_key_auth
 
